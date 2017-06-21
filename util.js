5c7f3fd0354072a2349121b7adfd11b429cf9297
Paul Pineda <paul@pineda.codes>
2017-06-21 15:36:54 +0100

    First commit

---
+++ util.js
@@ -0,0 +1,27 @@
+(function(exports){
+  'use strict'
+
+  function countPrototypesInChain(target){
+    var count=0;
+
+    function hasPrototype(o){
+      var proto = Object.getPrototypeOf(o);
+
+      if(!proto)
+        return count;
+
+      count++;
+      return hasPrototype(proto);
+    }
+
+    return hasPrototype(target);
+  }
+  
+  exports.utils = {
+    chainLength: chainLength
+  }
+}((typeof export === 'undefined')
+  ? window
+  : exports
+  )
+);
